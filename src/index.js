4f2f1f6c1e61254662e26d72641ac40f1b73995c
Stellamargy <stella.oloo@student.moringaschool.com>
2023-01-23 16:33:17 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router} from "react-router-dom";
 import App from "./components/App";
 
+
 ReactDOM.render(
-  <Router>
+  
+  < Router >
     <App />
-  </Router>,
-  document.getElementById("root")
+  </Router>
+ 
+ , document.getElementById("root")
 );
